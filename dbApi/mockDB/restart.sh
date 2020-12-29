89d31559937b08dff823d433a96b58ad0a29d90e
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2020-12-29 16:26:11 -0300

    setting the auth testing env

--- dbApi/mockDB/restart.sh
+++ dbApi/mockDB/restart.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-CONTAINER_NAME=mongodb
-docker exec $CONTAINER_NAME sh -c "mongo < /scripts/drop.js"
-docker exec $CONTAINER_NAME sh -c "mongo < /scripts/mockData.js"
+DB=mongo
+CACHE=redis
+docker exec $DB sh -c "mongo < /scripts/drop.js"
+docker exec $DB sh -c "mongo < /scripts/mockData.js"
+docker exec $CACHE sh -c "redis-cli FLUSHALL"
+docker exec $CACHE sh -c "redis-cli SET xxxx.yyyy.zzzz 3"
