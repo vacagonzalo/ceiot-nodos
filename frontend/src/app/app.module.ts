05f31cbbd89f844723dc79d12911602d6438291a
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2021-01-04 21:25:06 -0300

    skeleton finished

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { DevicesComponent } from './devices/devices.component';
+import { DeviceDetailComponent } from './device-detail/device-detail.component';
+import { LoginComponent } from './login/login.component';
+import { DeviceCalibrationComponent } from './device-calibration/device-calibration.component';
+import { DeviceReadingsComponent } from './device-readings/device-readings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    UsersComponent,
+    UserDetailComponent,
+    DevicesComponent,
+    DeviceDetailComponent,
+    LoginComponent,
+    DeviceCalibrationComponent,
+    DeviceReadingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
