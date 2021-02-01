6175018dc9417e7a38cefa34647a2d72ad516de9
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2021-01-31 22:23:24 -0300

    users done

--- frontend/src/app/new-user/new-user.component.ts
+++ frontend/src/app/new-user/new-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UsersService } from '../services/users.service';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-new-user',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userServ: UsersService) {
+    this.user = <User>{};
+  }
+
+  ngOnInit(): void { }
+
+  back() { 
+    this.router.navigate(['users']);
+  }
+
+  async newUser() {
+    await this.userServ.createUser(this.user);
+    this.back();
   }
 
 }
